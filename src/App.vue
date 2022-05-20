0bf0ac1b3a0b42bc2a205589c77bc82256245625
Simon Thordal <simonthordal@gmail.com>
2022-05-20 09:58:38 +0200

    First commit of Vue app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div> Hello world!</div>
+  </header>
+
+  <main>
+    You now have an app!
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
