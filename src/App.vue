e01f3ad32c29f596ebd9ea1933db302f66482438
Simon Thordal <simonthordal@gmail.com>
2022-05-21 10:43:37 +0200

    Add borders on 2nd side

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@ export default {
   data() {
     return {
       cards: [{
-        image_path: '/src/assets/images/test_image_rect.jpg',
+        image_path: '',
         title: 'Gloves of Being Mean',
         subtitle: 'Uncommon, Requires attunement',
         text: "London. Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's Inn Hall. Implacable November weather. As much mud in the streets as if the waters had but newly retired from the face of the earth, and it would not be wonderful to meet a Megalosaurus, forty feet long or so, waddling like an elephantine lizard up Holborn Hill."
       },{
-        image_path: '/src/assets/images/test_image_square.png',
+        image_path: '',
         title: 'Sentry Candle',
         subtitle: 'Uncommon',
         text: "This dark candle has a crenelated rim that remains even as the candle melts. While lit, small windows seemingly glow from within the candle's dark wax. The candle's magic is activated when the candle is lit, which requires an action. After burning for 8 hours, the candle is destroyed. You can snuff it out early for use at a later time. Moving the lit candle immediately snuffs it. Deduct the time it burned in increments of 1 minute from the candle's total burn time. While lit, the candle sheds dim light in a 15-foot radius. Any creature within that light is mentally alerted whenever another Tiny or larger creature enters the area within 60 feet of the candle."
@@ -29,6 +29,11 @@ export default {
           </div>
       </div>
     </div>
+    <div class="page-break cards-layout cardbacks-layout">
+      <div class="card-container" v-for="card in cards">
+        <div class="card-body cardback"></div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -45,10 +50,19 @@ export default {
     padding: 5px;
     overflow-y:clip;
 }
-
+.page-break {
+  page-break-before: always;
+}
+.cardback {
+  outline: 2px solid;
+}
 @media print {
   .cards-layout {
     display: flex;
+    flex-flow: row wrap;
+  }
+  .cardbacks-layout {
+    flex-flow: row-reverse wrap;
   }
 }
 </style>
