e4f02ea3782c72c25aa32ae2cd07a374e55a97f6
Simon Thordal <simonthordal@gmail.com>
2022-05-21 08:08:46 +0200

    Move card body to outer scope

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
-<script setup>
-import './components/Card.vue'
+<script>
 import Card from './components/Card.vue'
+export default {
+  components: {Card},
+  data() {
+    return {
+      cards: [{
+        image_path: '/src/assets/images/test_image_rect.jpg',
+        title: 'Gloves of Being Mean',
+        subtitle: 'Uncommon, Requires attunement',
+        text: "London. Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's Inn Hall. Implacable November weather. As much mud in the streets as if the waters had but newly retired from the face of the earth, and it would not be wonderful to meet a Megalosaurus, forty feet long or so, waddling like an elephantine lizard up Holborn Hill."
+      }]
+    }
+  }
+}
 </script>
 
 <template>
   <main>
-    <Card></Card>
+    <div class="card-container" v-for="card in cards">
+        <div class="card-body">
+          <Card v-bind="card"/>
+        </div>
+    </div>
   </main>
 </template>
 
 <style>
 @import './assets/base.css';
+
+.card-container {
+    padding: 5px
+}
+.card-body {
+    height: 475px;
+    width: 275px;
+    border: 2px solid;
+    padding: 5px;
+    overflow-y:clip;
+}
+
+@media print {
+}
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,31 @@
 <script>
 export default {
     data() {
-        return {
-            image_path: '/src/assets/images/test_image_rect.jpg'
-        }
-    }
+        
+    },
+    props: [
+        "image_path",
+        "title",
+        "subtitle",
+        "text"
+    ]
 }
 </script>
 <template>
-    <div class="card-container">
-        <div class="card-body">
-            <div class="image-container">
-                <img v-if="image_path" :src="image_path">
-            </div>
-            <div class="title-container">
-                <h3>Gloves of Being Mean</h3>
-            </div>
-            <div class="subtitle-container">
-                Uncommon, Requires attunement
-            </div>
-            <div class="text-container">
-                London. Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's Inn Hall. Implacable November weather. As much mud in the streets as if the waters had but newly retired from the face of the earth, and it would not be wonderful to meet a Megalosaurus, forty feet long or so, waddling like an elephantine lizard up Holborn Hill.
-            </div>
-        </div>
+    <div class="image-container">
+        <img v-if="image_path" :src="image_path">
+    </div>
+    <div class="title-container">
+        <h3>Gloves of Being Mean</h3>
+    </div>
+    <div class="subtitle-container">
+        Uncommon, Requires attunement
+    </div>
+    <div class="text-container">
+        London. Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's Inn Hall. Implacable November weather. As much mud in the streets as if the waters had but newly retired from the face of the earth, and it would not be wonderful to meet a Megalosaurus, forty feet long or so, waddling like an elephantine lizard up Holborn Hill.
     </div>
 </template>
 <style>
-.card-container {
-    padding: 5px
-}
-.card-body {
-    height: 475px;
-    width: 275px;
-    border: 2px solid;
-    padding: 5px;
-    overflow-y:clip;
-}
 .image-container {
     max-height: 200px;
 }
@@ -44,11 +34,11 @@ export default {
     object-fit: scale-down;
 }
 .title-container {
-    padding: 2px 0;
+    padding-top: 2px;
     font: 1.2em "Bookinsanity";
 }
 .subtitle-container {
-    padding: 2px 0;
+    padding-bottom: 4px;
     font: italic 1em "Bookinsanity";
 }
 .text-container {
