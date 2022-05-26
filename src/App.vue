b5a9f4bb463d0fc82c89ca496ad44a3eb25aa7be
Simon Thordal <simonthordal@gmail.com>
2022-05-26 13:28:19 +0200

    Calculate offset per card

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ export default {
   components: {Card},
   data() {
     return {
-      offsetTop: {
+      offsets: [],
+      defaultOffset: {
         top: -1
       },
       defaultCard: {
@@ -20,14 +21,16 @@ export default {
     this.addCard()
   },
   methods: {
-    positionCardback(size) {
-      this.offsetTop.top = -size + "px"
+    positionCardback(i, size) {
+      let offset = this.offsets[i]
+      offset.top = -size + "px"
     },
     addCard() {
       if (this.cards.length >= 4) {
         alert('You cannot add more than 4 cards for now')
         return;
       }
+      this.offsets.push({...this.defaultOffset})
       this.cards.push({...this.defaultCard})
     },
     updateProperty(card_index, property, value) {
@@ -42,16 +45,16 @@ export default {
   <main>
     <div class="cards-layout">
       <div class="card-body" v-for="(card, i) in cards">
-        <Card v-bind="card" @text-box-size="positionCardback" @property-update="updateProperty(i, ...$event)"/>
+        <Card v-bind="card" @text-box-size="positionCardback(i, $event)" @property-update="updateProperty(i, ...$event)"/>
       </div>
       <div class="add-button" @click="addCard">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/></svg>
       </div>
     </div>
     <div class="page-break cards-layout cardbacks-layout">
-      <div class="card-body cardback" v-for="card in cards">
+      <div class="card-body cardback" v-for="(card, i) in cards">
         <div class="top-border"></div>
-        <div class="offset-text" :style="offsetTop">
+        <div class="offset-text" :style="offsets[i]">
           {{card.text}}
         </div>
       </div>
